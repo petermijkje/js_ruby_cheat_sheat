07417569b8bcf3284e7bdba7f3dc3516c4e2749f
Peter Sarll <petersarll@gmail.com>
2020-02-12 23:17:00 -0800

    creating two different files to compare JavaScript to Ruby

---
+++ ruby.rb
@@ -0,0 +1,25 @@
+#### Ruby Cheat Sheet##
+#######################
+
+
+
+
+# to comment out use # :)
+
+=begin to comment 
+out multiple lines
+use this
+=end
+
+# printing argument / 
+puts "argument"
+
+
+# creating functions 
+def newVariable(a,b) 
+    return (a + b)
+end
+
+puts newVariable(1, 3)
+
+
